f2efbddcbfc5fb54f840c134b4fc13b5dd0b8a56
Vansh Kumar Sharma <vns.vansh123@gmail.com>
2023-03-03 00:29:11 +0530

    Removed GET request form API

--- SignopediaEntry/views.py
+++ SignopediaEntry/views.py
@@ -24,28 +24,6 @@ class call_model(APIView):
         self.model_name="model.h5"
         self.loaded_model=tf.keras.models.load_model(os.path.join(settings.MODEL_ROOT,self.model_name))
         
-    def get(self,request):
-        if request.method == 'GET':
-            
-            # sentence is the query we want to get the prediction for
-            # params =  request.GET.get('qry')
-            im=Image.open(os.path.join(settings.MEDIA_ROOT,"image.png"))
-            im=im.resize((30,30))
-            im=img_to_array(im)
-            im=np.expand_dims(im,axis=0)
-            print("Hitted On the URL------------------------------->>>")
-        
-            # serializer = CustomJSONSerializer()
-            
-            
-            # predict method used to get the prediction
-            response = self.loaded_model.predict(im)
-            
-            # return JsonResponse(serializer.serialize(["hello_world"]), safe=False)
-            
-            # returning JSON response
-            return JsonResponse({"class":str(np.argmax(response)+1)})
-        
     def post(self, request, *args, **kwargs):
         posts_serializer = PostSerializer(data=request.data)
         print(request.data["image"])
